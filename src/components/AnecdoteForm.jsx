ecf6278442b27d9fd752a9f5546a396f5ca2d6bd
hamesomer11 <hamzaomer45@gmail.com>
2024-09-18 23:18:33 +0100

    query-anecdotes: Exercise 6.22

--- src/components/AnecdoteForm.jsx
+++ src/components/AnecdoteForm.jsx
@@ -16,7 +16,7 @@ const AnecdoteForm = () => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    newAnecdoteMutation.mutate({ content, vote: 0 })
+    newAnecdoteMutation.mutate({ content, votes: 0 })
   }
 
   return (
